93ab92072c47c773c468e74266e365ff63f8c37e
ajh123 <ajh123@users.noreply.github.com>
2022-05-02 12:52:17 +0100

    Probably fix mirror changing? And again?

--- src/me/mrletsplay/shittyauthpatcher/version/MinecraftVersion.java
+++ src/me/mrletsplay/shittyauthpatcher/version/MinecraftVersion.java
@@ -26,7 +26,7 @@ public class MinecraftVersion implements JSONConvertible {
 
 	// TODO: java runtime: https://launchermeta.mojang.com/v1/products/java-runtime/2ec0cc96c44e5a76b9c8b7c39df7210883d12871/all.json
 
-	public static final List<MinecraftVersion> VERSIONS = new ArrayList<>();
+	public static List<MinecraftVersion> VERSIONS = new ArrayList<>();
 	public static MinecraftVersion LATEST_RELEASE;
 	public static MinecraftVersion LATEST_SNAPSHOT;
 
