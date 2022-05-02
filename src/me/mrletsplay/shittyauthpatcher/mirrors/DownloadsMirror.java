cbb9694a8a97b558e541ff1e8a9cef953e482f63
ajh123 <ajh123@users.noreply.github.com>
2022-05-02 10:39:54 +0100

    Add names to mirrors

--- src/me/mrletsplay/shittyauthpatcher/mirrors/DownloadsMirror.java
+++ src/me/mrletsplay/shittyauthpatcher/mirrors/DownloadsMirror.java
@@ -6,6 +6,9 @@ import me.mrletsplay.mrcore.json.converter.JSONValue;
 
 public class DownloadsMirror implements JSONConvertible {
 	@JSONValue
+	public String name;
+
+	@JSONValue
 	public String version_manifest;
 
 	@JSONValue
@@ -14,7 +17,8 @@ public class DownloadsMirror implements JSONConvertible {
 	@JSONConstructor
 	public DownloadsMirror() {}
 
-	public DownloadsMirror(String version_manifest, String assets_url) {
+	public DownloadsMirror(String name, String version_manifest, String assets_url) {
+		this.name = name;
 		this.version_manifest = version_manifest;
 		this.assets_url = assets_url;
 	}
--- src/me/mrletsplay/shittyauthpatcher/mirrors/MojangMirror.java
+++ src/me/mrletsplay/shittyauthpatcher/mirrors/MojangMirror.java
@@ -2,6 +2,6 @@ package me.mrletsplay.shittyauthpatcher.mirrors;
 
 public class MojangMirror extends DownloadsMirror {
     public MojangMirror(){
-        super("http://launchermeta.mojang.com/mc/game/version_manifest.json", "http://resources.download.minecraft.net/");
+        super("Mojang","http://launchermeta.mojang.com/mc/game/version_manifest.json", "http://resources.download.minecraft.net/");
     }
 }
