73b753050e06a56636ddc0a663a317725989c1d0
ajh123 <ajh123@users.noreply.github.com>
2022-05-02 20:22:24 +0100

    Fixed?

--- src/me/mrletsplay/shittyauthpatcher/mirrors/MojangMirror.java
+++ src/me/mrletsplay/shittyauthpatcher/mirrors/MojangMirror.java
@@ -1,6 +1,11 @@
 package me.mrletsplay.shittyauthpatcher.mirrors;
 
-public class MojangMirror extends DownloadsMirror {
+import me.mrletsplay.mrcore.json.converter.JSONConstructor;
+import me.mrletsplay.mrcore.json.converter.JSONConvertible;
+
+public class MojangMirror extends DownloadsMirror implements JSONConvertible {
+
+    @JSONConstructor
     public MojangMirror(){
         super("Mojang","http://launchermeta.mojang.com/mc/game/version_manifest.json", "http://resources.download.minecraft.net/");
     }
