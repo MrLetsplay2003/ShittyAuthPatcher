73677abf2092a42f247a543e4444e2bb27bc7a96
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2022-05-31 20:42:49 +0200

    Fix generated library paths

--- src/me/mrletsplay/shittyauthpatcher/version/meta/Library.java
+++ src/me/mrletsplay/shittyauthpatcher/version/meta/Library.java
@@ -1,24 +1,27 @@
 package me.mrletsplay.shittyauthpatcher.version.meta;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import me.mrletsplay.mrcore.json.JSONArray;
 import me.mrletsplay.mrcore.json.JSONObject;
 
 public class Library {
-	
+
 	private JSONObject library;
-	
+
 	public Library(JSONObject library) {
 		this.library = library;
 	}
-	
+
 	public String getName() {
 		return library.getString("name");
 	}
-	
+
 	public JSONArray getRules() {
 		return library.optJSONArray("rules").orElse(null);
 	}
-	
+
 	public DownloadableFile getArtifactDownload() {
 		if(!library.has("downloads")) {
 			if(library.has("url")) {
@@ -35,7 +38,7 @@ public class Library {
 		String url = artifact.getString("url");
 		return new DownloadableFile(path, url);
 	}
-	
+
 	public DownloadableFile getNativesDownload(String osName) {
 		if(!library.has("natives")) return null;
 		JSONObject natives = library.getJSONObject("natives");
@@ -45,12 +48,12 @@ public class Library {
 		String nativesURL = nativeLib.getString("url");
 		return new DownloadableFile(nativesPath, nativesURL);
 	}
-	
+
 	public String getGeneratedPath() {
 		String name = getName();
 		String[] spl = name.split(":");
-		
-		return spl[0].replace('.', '/') + "/" + spl[1] + "/" + spl[2] + "/" + spl[1] + "-" + spl[2] + ".jar";
+
+		return spl[0].replace('.', '/') + "/" + spl[1] + "/" + spl[2] + "/" + Arrays.stream(spl).skip(1).collect(Collectors.joining("-")) + ".jar";
 	}
 
 }
