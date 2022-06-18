bda4b8b53af44601731f8e6d97d1c7f6b32e76fc
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2022-06-18 21:26:31 +0200

    Add ServerConfiguration#hashString()

--- src/me/mrletsplay/shittyauthpatcher/util/ServerConfiguration.java
+++ src/me/mrletsplay/shittyauthpatcher/util/ServerConfiguration.java
@@ -1,13 +1,18 @@
 package me.mrletsplay.shittyauthpatcher.util;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
 
 import me.mrletsplay.mrcore.json.converter.JSONConstructor;
 import me.mrletsplay.mrcore.json.converter.JSONConvertible;
 import me.mrletsplay.mrcore.json.converter.JSONValue;
+import me.mrletsplay.mrcore.misc.ByteUtils;
+import me.mrletsplay.mrcore.misc.FriendlyException;
 
 public class ServerConfiguration implements JSONConvertible {
-	
+
 	@JSONValue
 	public String
 		authServer,
@@ -15,7 +20,7 @@ public class ServerConfiguration implements JSONConvertible {
 		sessionServer,
 		servicesServer,
 		skinHost;
-	
+
 	@JSONConstructor
 	public ServerConfiguration() {}
 
@@ -25,6 +30,21 @@ public class ServerConfiguration implements JSONConvertible {
 		this.sessionServer = sessionServer;
 		this.servicesServer = servicesServer;
 		this.skinHost = skinHost;
+	}
+
+	/**
+	 * Computes a hash string of all servers in this configuration.
+	 * This is used to distinguish different configurations without the need for keeping track of all value, e.g. for keeping files related to this server configuration.
+	 * @return A hexadecimal MD5 hash string of this configuration
+	 */
+	public String hashString() {
+		String theString = authServer + "\n" + accountsServer + "\n" + sessionServer + "\n" + servicesServer + "\n" + skinHost;
+		try {
+			MessageDigest d = MessageDigest.getInstance("MD5");
+			return ByteUtils.bytesToHex(d.digest(theString.getBytes(StandardCharsets.UTF_8)));
+		} catch (NoSuchAlgorithmException e) {
+			throw new FriendlyException("Failed to hash values", e);
+		}
 	}
 
 	@Override
@@ -47,5 +67,5 @@ public class ServerConfiguration implements JSONConvertible {
 				&& Objects.equals(sessionServer, other.sessionServer)
 				&& Objects.equals(skinHost, other.skinHost);
 	}
-	
+
 }
